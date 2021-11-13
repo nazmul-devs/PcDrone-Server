0d7509ab418f2f42f46f47110c1b317b10ab3025
Nazmul Hosen <nazmulhosenm668@gmail.com>
2021-11-13 13:44:15 +0600

    verify email with jwt token

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const admin = require("firebase-admin");
 const ObjectId = require("mongodb").ObjectId;
 const { MongoClient } = require("mongodb");
 require("dotenv").config();
@@ -8,12 +9,33 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// jwt
+
+const serviceAccount = require("./pcdrone-adminsdk.json");
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+});
+
 // mongoDb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.f4mgp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
 	useNewUrlParser: true,
 	useUnifiedTopology: true,
 });
+
+// token varify
+async function varifyToken(req, res, next) {
+	if (req.headers?.authorization?.startsWith("Bearer ")) {
+		const token = req.headers.authorization.split(" ")[1];
+
+		try {
+			const decodetUser = await admin.auth().verifyIdToken(token);
+			req.decodetEmail = decodetUser.email;
+		} catch {}
+	}
+	next();
+}
 
 async function run() {
 	try {
@@ -115,7 +137,6 @@ async function run() {
 			const review = req.body;
 			const result = await reviewsCollection.insertOne(review);
 			res.json(result);
-			console.log(result);
 		});
 
 		// get review
@@ -125,12 +146,25 @@ async function run() {
 		});
 
 		// Make admin
-		app.put("/users/admin", async (req, res) => {
+		app.put("/users/admin", varifyToken, async (req, res) => {
 			const email = req.body.email;
-			const filter = { email: email };
-			const updateDoc = { $set: { role: "admin" } };
-			const result = await usersCollection.updateOne(filter, updateDoc);
-			res.json(result);
+			const requister = req.decodetEmail;
+			if (requister) {
+				const requisterAccount = await usersCollection.findOne({
+					email: requister,
+				});
+				if (requisterAccount.role === "admin") {
+					const filter = { email: email };
+					const updateDoc = { $set: { role: "admin" } };
+					const result = await usersCollection.updateOne(
+						filter,
+						updateDoc
+					);
+					res.json(result);
+				} else {
+					res.status(403).json({ message: "you do not have access" });
+				}
+			}
 		});
 		// verify admin
 		app.get("/users/:email", async (req, res) => {
